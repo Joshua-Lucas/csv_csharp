1d4f47c2e941d94c313f8c277772eaea3f2877a2
Joshua Lucas <joshuamlucas91@gmail.com>
2021-03-04 11:47:47 -0500

    init the project and set it up

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace reviewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Pass a CSV file to a method that reads each line of csv
+
+            //pharse that line and create a new isntace of Tranasction object with pharsed data
+
+
+            // store data in collection
+
+            //loop through collection and store in DB
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C# project
+
+This console app's goal is to pull in a CSV file read it, pharse it, and then store it's data on a db.
